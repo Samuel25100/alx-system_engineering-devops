5efd10cf2d146c6ed96ebef91e49efec1f960afd
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-19 16:59:33 +0300

    add file 1-install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install flask from pip3 with version 2.1.0
+# install flask from pip3 with version 2.1.0
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
