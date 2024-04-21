96fe4c14400c56f7466d29fd8d22aac2440a5c56
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-21 10:32:22 +0300

    add file 1-install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # install flask from pip3 with version 2.1.0
 
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure  => installed,
+  require => Exec['install_flask']
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip install flask=2.0.1',
 }
