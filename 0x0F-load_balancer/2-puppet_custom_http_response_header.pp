e9679bd216bf8af088fbf295328a7e1fcc013b75
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-29 20:11:26 +0300

    add file 2-puppet_custom

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# task #0, we’d like you to automate the task of creating a
+# custom HTTP header response
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/etc/nginx/sites-available/default':
+  ensure => 'file'
+}
+
+file_line{'X-Served-By':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  line    => '    server_name _;
+    add_header X-Served-By $hostname;',
+  match   => 'server_name _;',
+  require => Exec['sudo service nginx']
+}
