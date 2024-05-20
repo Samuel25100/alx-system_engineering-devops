06555a300c2a6c924e8be0cad9e9be144cb9d35a
Samuel25100 <samuelgselassie1@gmail.com>
2024-05-20 16:12:55 +0300

    add file 0 and readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Return information from api about employee todo list progress."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    em_id = int(sys.argv[1])
+    tot = 0
+    done = 0
+    title = []
+    url = f"https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    result = response.json()
+    url = f"https://jsonplaceholder.typicode.com/users/{em_id}"
+    response1 = requests.get(url)
+    user = response1.json()
+    user_n = user['name']
+    for dic in result:
+        if dic["userId"] == em_id:
+            tot += 1
+            if dic['completed'] is True:
+                done += 1
+                title.append(dic['title'])
+    print(f"Employee {user_n} is done with tasks({done}/{tot})")
+    for line in title:
+        print(f"\t{line}")
