9984a62f62ec6e2ef8c4e223420be2a6967ed9a8
Samuel25100 <samuelgselassie1@gmail.com>
2024-06-04 15:21:14 +0300

    add file 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,8 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     param = {"limit": 10}
-    response = requests.get(url, headers=headers, params=param, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=param, allow_redirects=False)
 
     if response.status_code == 200:
         result = response.json()
