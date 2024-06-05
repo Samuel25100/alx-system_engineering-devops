49151776ccc5b1faff504c3915a46eb94d722b1f
Samuel25100 <samuelgselassie1@gmail.com>
2024-06-05 20:41:21 +0300

    add file 0-starce

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Fix apache error of php
 exec {'echo foo':
-command => '/bin/sed -i "s|/class-wp-locale.phpp|/class-wp-locale.php|" /var/www/html/wp-settings.php'
+command => '/bin/sed -i "s|class-wp-locale.phpp|class-wp-locale.php|" /var/www/html/wp-settings.php'
 }
 
 service {'apache2':
