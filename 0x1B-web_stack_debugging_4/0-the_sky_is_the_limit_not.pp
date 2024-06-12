a98abba8dcb1bc0644e6982796f26b4b4950c264
Samuel25100 <samuelgselassie1@gmail.com>
2024-06-12 14:21:36 +0300

    add file 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
 #Configure nginx to accept more user 10000
-file_line { 'increase_worker_connections':
-  path  => '/etc/nginx/nginx.conf',
-  line  => 'worker_connections 10000;',
-  match => '^worker_connections\s+\d+;$',
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Service['nginx'],
 }
 
 service { 'nginx':
   ensure     => 'running',
   enable     => true,
-  subscribe  => File['/etc/nginx/nginx.conf'],
 }
